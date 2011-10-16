23ab6c28ef10b7d73f8f98912683fc9eaef723bb
Philip Pemberton <philpem@philpem.me.uk>
2011-10-16 01:04:43 +0100

    update idplatform to fix a nasty bug on anything that isn't OSX
    idplatform.sh wouldn't run on anything that wasn't Mac OS X. Fixed.

--- idplatform.sh
+++ idplatform.sh
@@ -2,9 +2,9 @@
 
 UNAME_S=`uname -s`
 
-if [ x$UNAME_S == "xDarwin" ]; then
+if [ "x$UNAME_S" = "xDarwin" ]; then
 	echo osx
-elif [x$UNAME_S == "xLinux" ]; then
+elif [ "x$UNAME_S" = "xLinux" ]; then
 	echo linux
 else
 	echo unknown
